887bf105c670de9f31e576225a5f1f77300ed74d
Elorm-cmd <elormmegamind@gmail.com>
2023-08-27 22:08:27 +0000

    Update routes.py

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Blueprint
 import requests
 
-app = Flask(__name__)
+main = Blueprint('main', __name__)
 
-@app.route('/your_route', methods=['POST'])
+@main.route('/attendance', methods=['POST'])
 def your_route():
     # Get the JSON data from the POST request
     data = request.get_json()
@@ -13,24 +13,15 @@ def your_route():
         # Get the value of 'id'
         id_value = data['id']
 
-        # Perform any desired operations with the 'id' value
-        # For this example, we'll simply return it as a response
-        response_data = {'message': f'Received id: {id_value}'}
+        # Form the URL for the external API with the 'id' value
+        external_api_url = f'http://goldberg.pythonanywhere.com/api/attendances/record/2?id={id_value}'
 
-        return jsonify(response_data), 200
+        # Send a POST request to the external API without a request body
+        response = requests.post(external_api_url)
+
+        if response.status_code == 200:
+            return jsonify({'message': f'Successfully recorded attendance for id: {id_value}'}), 200
+        else:
+            return jsonify({'error': f'Failed to record attendance for id: {id_value}'}), 500
     else:
         return jsonify({'error': 'Missing or invalid "id" in the JSON data'}), 400
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-# Make a POST request to the Flask route
-flask_url = "http://localhost:5000/your_route"
-response = requests.post(flask_url, json={"id": "x"})
-
-# Check the response
-if response.status_code == 200:
-    print("POST request to Flask was successful")
-    print("Response:", response.json())
-else:
-    print("POST request to Flask failed with status code:", response.status_code)
